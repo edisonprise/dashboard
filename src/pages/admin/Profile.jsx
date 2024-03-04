7e1359aa90055e70ac702cfa341a2d752698aa5d
Edinson <edisonprise@hotmail.com>
2024-03-03 21:56:10 -0500

    Profile Page

--- src/pages/admin/Profile.jsx
+++ src/pages/admin/Profile.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 //Icons
-import { RiEdit2Line, RiShieldCheckLine } from "react-icons/ri";
+import {
+  RiEdit2Line,
+  RiShieldCheckLine,
+  RiErrorWarningLine,
+} from "react-icons/ri";
+import { Link } from "react-router-dom";
+import { Switch } from "@headlessui/react";
 
 const Profile = () => {
+  const [enabled, setEnabled] = useState(false);
   return (
     <>
+      {/*Profile */}
       <div className="bg-secondary-100 p-8 rounded-xl mb-8">
         <h1 className="text-xl text-gray-100">Profile</h1>
         <hr className="my-8 border-gray-500/30" />
@@ -25,14 +34,14 @@ const Profile = () => {
                   <RiEdit2Line />
                 </label>
                 <input type="file" className="hidden" id="avatar" />
-                <p className="text-gray-500 text-sm">
-                  Allowed file types: png, jpg, jpeg
-                </p>
               </div>
+              <p className="text-gray-500 text-sm">
+                Allowed file types: png, jpg, jpeg
+              </p>
             </div>
           </div>
-          <div className="flex items-center mb-8">
-            <div className="w-1/4">
+          <div className="flex flex-col gap-y-2 md:flex-row md:items-center mb-8">
+            <div className="w-full md:w-1/4">
               <p className="">
                 {" "}
                 Full Name<span className="text-red-500">*</span>
@@ -55,8 +64,8 @@ const Profile = () => {
               </div>
             </div>
           </div>
-          <div className="flex items-center mb-8">
-            <div className="w-1/4">
+          <div className="flex flex-col md:flex-row md:items-center gap-y-2 mb-8">
+            <div className="w-full md:w-1/4">
               <p className="">
                 {" "}
                 Name of the Company<span className="text-red-500">*</span>
@@ -70,8 +79,8 @@ const Profile = () => {
               />
             </div>
           </div>
-          <div className="flex items-center mb-8">
-            <div className="w-1/4">
+          <div className="flex flex-col md:flex-row md:items-center gap-y-2 mb-8">
+            <div className="w-full md:w-1/4">
               <p className="">
                 {" "}
                 Contact phone<span className="text-red-500">*</span>
@@ -85,8 +94,8 @@ const Profile = () => {
               />
             </div>
           </div>
-          <div className="flex items-center mb-8">
-            <div className="w-1/4">
+          <div className="flex flex-col md:flex-row md:items-center gap-y-2 mb-8">
+            <div className="w-full md:w-1/4">
               <p className="">
                 {" "}
                 Company Site<span className="text-red-500">*</span>
@@ -100,8 +109,8 @@ const Profile = () => {
               />
             </div>
           </div>
-          <div className="flex items-center mb-8">
-            <div className="w-1/4">
+          <div className="flex flex-col md:flex-row md:items-center gap-y-2 mb-8">
+            <div className="w-full md:w-1/4">
               <p className="">
                 {" "}
                 Country<span className="text-red-500">*</span>
@@ -109,9 +118,7 @@ const Profile = () => {
             </div>
             <div className="flex-1 ">
               <select className="w-full py-2 px-4 outline-none rounded-lg bg-secondary-900 appearance-none">
-                <option value="Argentina" selected>
-                  Argentina
-                </option>
+                <option defaultValue="Argentina">Argentina</option>
                 <option value="Brazil">Brazil</option>
                 <option value="Colombia">Colombia</option>
                 <option value="Mexico">Mexico</option>
@@ -120,8 +127,8 @@ const Profile = () => {
               </select>
             </div>
           </div>
-          <div className="flex items-center mb-8">
-            <div className="w-1/4">
+          <div className="flex flex-col md:flex-row md:items-center gap-y-2 mb-8">
+            <div className="w-full md:w-1/4">
               <p className="">
                 {" "}
                 City<span className="text-red-500">*</span>
@@ -129,9 +136,7 @@ const Profile = () => {
             </div>
             <div className="flex-1 ">
               <select className="w-full py-2 px-4 outline-none rounded-lg bg-secondary-900 appearance-none">
-                <option value="Buenos Aires" selected>
-                  Buenos Aires
-                </option>
+                <option defaultValue="Buenos Aires">Buenos Aires</option>
                 <option value="Brazilia">Brazilia</option>
                 <option value="Bogota">Bogota</option>
                 <option value="Mexico-city">Mexico City</option>
@@ -148,29 +153,30 @@ const Profile = () => {
           </button>
         </div>
       </div>
-      <div className="bg-secondary-100 p-8 rounded-xl">
+      {/*Change password*/}
+      <div className="bg-secondary-100 p-8 rounded-xl mb-8">
         <h1 className="text-xl text-gray-100">Sign-in Method</h1>
         <hr className="my-8 border-gray-500/30" />
         <form className="mb-8">
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col md:flex-row md:items-center gap-y-4 justify-between">
             <div>
               <h5 className="text-gray-100 text-xl mb-1">Email</h5>
               <p className="text-gray-500 text-sm">edisonprise@hotmail.com</p>
             </div>
             <div>
-              <button className="bg-secondary-900/50 py-3 px-4 rounded-lg hover:bg-secondary-900 hover: text-gray-100 transition-colors ">
+              <button className="w-full md:w-auto bg-secondary-900/50 py-3 px-4 rounded-lg hover:bg-secondary-900 hover:text-gray-100 transition-colors ">
                 Change Email
               </button>
             </div>
           </div>
           <hr className="my-8 border-gray-500/30 border-dashed" />
-          <div className="flex items-center justify-between">
+          <div className="flex flex-col md:flex-row md:items-center gap-y-4 justify-between">
             <div>
               <h5 className="text-gray-100 text-xl mb-1">Password</h5>
               <p className="text-gray-500 text-sm">************</p>
             </div>
             <div>
-              <button className="bg-secondary-900/50 py-3 px-4 rounded-lg hover:bg-secondary-900 hover: text-gray-100 transition-colors ">
+              <button className="w-full md:w-auto bg-secondary-900/50 py-3 px-4 rounded-lg hover:bg-secondary-900 hover:text-gray-100 transition-colors ">
                 Change Password
               </button>
             </div>
@@ -194,6 +200,261 @@ const Profile = () => {
               Enable
             </button>
           </div>
+        </div>
+      </div>
+      {/*Connected Accounts */}
+      <div className="bg-secondary-100 p-8 rounded-xl mb-8">
+        <h1 className="text-xl text-gray-100">Connected Accounts</h1>
+        <hr className="my-8 border-gray-500/30" />
+        <div className="flex flex-col md:flex-row gap-4 items-center bg-green-600/10 p-4 rounded-lg border border-dashed border-green-600 mb-8">
+          <div className="flex-justify-center">
+            <RiShieldCheckLine className="text-5xl text-green-500" />
+          </div>
+          <div className="flex-1">
+            <p className="text-gray-500">
+              Two-factor authentication adds an extra layer of security to your
+              account. To log in, in addition you´ll need to provide a 4 digit
+              amazing code.
+              <Link to="/" className="text-green-400">
+                Learn More
+              </Link>
+            </p>
+          </div>
+        </div>
+        <form className="mb-8">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png"
+                className="w-8 h-8 object-cover"
+              />
+              <div className="flex flex-col gap-y-1">
+                <h5 className="text-gray-100">Google</h5>
+                <p className="text-gray-500 text-sm">
+                  Plan properly your workflow
+                </p>
+              </div>
+            </div>
+            <div>
+              <Switch
+                checked={enabled}
+                onChange={setEnabled}
+                className={`${
+                  enabled ? "bg-primary" : "bg-secondary-900"
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+              >
+                <span className="sr-only">Enable notifications</span>
+                <span
+                  className={`${
+                    enabled
+                      ? "translate-x-6 bg-secondary-900"
+                      : "translate-x-1 bg-gray-500"
+                  } inline-block h-4 w-4 transform rounded-full transition`}
+                />
+              </Switch>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <img
+                src="https://pngimg.com/d/github_PNG40.png"
+                className="w-8 h-8 object-cover"
+              />
+              <div className="flex flex-col gap-y-1">
+                <h5 className="text-gray-100">GitHub</h5>
+                <p className="text-gray-500 text-sm">
+                  Keep eye on your Repositories
+                </p>
+              </div>
+            </div>
+            <div>
+              <Switch
+                checked={enabled}
+                onChange={setEnabled}
+                className={`${
+                  enabled ? "bg-primary" : "bg-secondary-900"
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+              >
+                <span className="sr-only">Enable notifications</span>
+                <span
+                  className={`${
+                    enabled
+                      ? "translate-x-6 bg-secondary-900"
+                      : "translate-x-1 bg-gray-500"
+                  } inline-block h-4 w-4 transform rounded-full transition`}
+                />
+              </Switch>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Slack_icon_2019.svg/2048px-Slack_icon_2019.svg.png"
+                className="w-8 h-8 object-cover"
+              />
+              <div className="flex flex-col gap-y-1">
+                <h5 className="text-gray-100">Slack</h5>
+                <p className="text-gray-500 text-sm">
+                  Integrate Projects Discussions
+                </p>
+              </div>
+            </div>
+            <div>
+              <Switch
+                checked={enabled}
+                onChange={setEnabled}
+                className={`${
+                  enabled ? "bg-primary" : "bg-secondary-900"
+                } relative inline-flex h-6 w-11 items-center rounded-full`}
+              >
+                <span className="sr-only">Enable notifications</span>
+                <span
+                  className={`${
+                    enabled
+                      ? "translate-x-6 bg-secondary-900"
+                      : "translate-x-1 bg-gray-500"
+                  } inline-block h-4 w-4 transform rounded-full transition`}
+                />
+              </Switch>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+        </form>
+      </div>
+      {/*Email preferences*/}
+      <div className="bg-secondary-100 p-8 rounded-xl mb-8">
+        <h1 className="text-xl text-gray-100">Email Preferences</h1>
+        <hr className="my-8 border-gray-500/30" />
+        <form className="mb-8">
+          <div className="flex items-center gap-4">
+            <input type="checkbox" className="accent-primary" id="id1" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id1" className="text-gray-100">
+                Succesful Payments
+              </label>
+              <p className="text-gray-500 text-sm">
+                Receive a notification for every succesful payment.
+              </p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <div className="flex items-center gap-4">
+            <input type="checkbox" className="accent-primary" id="id2" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id2" className="text-gray-100">
+                Payouts
+              </label>
+              <p className="text-gray-500 text-sm">
+                Receive a notification for every initiated payout.
+              </p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <div className="flex items-center gap-4">
+            <input type="checkbox" className="accent-primary" id="id3" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id3" className="text-gray-100">
+                Free Collection
+              </label>
+              <p className="text-gray-500 text-sm">
+                Receive a notification each time you collect a fee from sales.
+              </p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <div className="flex items-center gap-4">
+            <input type="checkbox" className="accent-primary" id="id4" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id4" className="text-gray-100">
+                Customer Payment Dispute
+              </label>
+              <p className="text-gray-500 text-sm">
+                Receive a notification if a payment is disputed by a customet
+                and for dispute purposes.
+              </p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <div className="flex items-center gap-4">
+            <input type="checkbox" className="accent-primary" id="id5" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id5" className="text-gray-100">
+                Refund Alerts
+              </label>
+              <p className="text-gray-500 text-sm">
+                Receive a notification if a pyment is stated as ridk by the
+                Finance Department.
+              </p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <div className="flex items-center gap-4">
+            <input type="checkbox" className="accent-primary" id="id6" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id6" className="text-gray-100">
+                Invoice Payments
+              </label>
+              <p className="text-gray-500 text-sm">
+                Receive a notification if a customer sends an incorrect amount
+                to pay their invoice.
+              </p>
+            </div>
+          </div>
+          <hr className="my-8 border-gray-500/30 border-dashed" />
+          <div className="flex items-center gap-4">
+            <input type="checkbox" className="accent-primary" id="id6" />
+            <div className="flex flex-col gap-y-1">
+              <label htmlFor="id7" className="text-gray-100">
+                Webhook API Endpoints
+              </label>
+              <p className="text-gray-500 text-sm">
+                Receive notifications for consistently failing webhook API
+                endpoints.
+              </p>
+            </div>
+          </div>
+        </form>
+        <hr className="my-8 border-gray-500/30 border-dashed" />
+        <div className="flex justify-end">
+          <button className="bg-primary/80 text-black py-2 px-4 rounded-lg hover:bg-primary transition-colors">
+            Save
+          </button>
+        </div>
+      </div>
+      {/*Deactivate Accounts*/}
+      <div className="bg-secondary-100 p-8 rounded-xl mb-8">
+        <h1 className="text-xl text-gray-100">Deactivate Account</h1>
+        <hr className="my-8 border-gray-500/30" />
+        <div className="flex flex-col md:flex-row items-center gap-4 bg-yellow-600/10 p-4 rounded-lg border border-dashed border-yellow-600 mb-8">
+          <div className="flex-justify-center">
+            <RiErrorWarningLine className="text-5xl text-yellow-600" />
+          </div>
+          <div className="flex-1">
+            <h5 className="text-gray-100 text-xl mb-2">
+              You are deactivating your account
+            </h5>
+            <p className="text-gray-500">
+              For extra security, this requires you to confirm your email or
+              phone number when you reset your password.
+              <Link to="/" className="text-blue-500">
+                Learn More
+              </Link>
+            </p>
+          </div>
+        </div>
+        <form className="flex items-center gap-4">
+          <input type="checkbox" className="accent-primary" id="idInactive" />
+          <label htmlFor="idInactive" className="text-gray-500">
+            I confirm my account deactivation
+          </label>
+        </form>
+        <hr className="my-8 border-gray-500/30" />
+        <div className="flex justify-end">
+          <button className="bg-red-500/80 text-gray-100 py-2 px-4 rounded-lg hover:bg-red-500 transition-colors">
+            Deactivate Account
+          </button>
         </div>
       </div>
     </>
