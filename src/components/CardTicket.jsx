9e755ab05013c9770062e812c82d2e332bf5f128
Edinson <edisonprise@hotmail.com>
2024-03-07 16:58:42 -0500

    end of the project

--- src/components/CardTicket.jsx
+++ src/components/CardTicket.jsx
@@ -1,60 +1,95 @@
-import { RiTicketLine, RiMore2Fill } from "react-icons/ri";
+import PropTypes from "prop-types";
+import { RiTicketLine, RiMore2Fill, RiAddLine } from "react-icons/ri";
 import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
 import "@szhsin/react-menu/dist/index.css";
 import "@szhsin/react-menu/dist/transitions/slide.css";
 import { Link } from "react-router-dom";
 
+const CardTicket = ({ ticket, totalTickets, text }) => {
+  let status = "";
+  let textColor = "";
 
-const CardTicket = () => {
+  switch (ticket) {
+    case "pending":
+      status = "bg-yellow-500/10 text-yellow-500";
+      textColor = "text-yellow-500";
+      break;
+    case "inProcess":
+      status = "bg-blue-500/10 text-blue-500";
+      textColor = "text-blue-500";
+      break;
+    case "closed":
+      status = "bg-green-500/10 text-green-500";
+      textColor = "text-green-500";
+      break;
+    case "total":
+      status = "bg-pink-500/10 text-pink-500";
+      textColor = "text-pink-500";
+      break;
+  }
   return (
-    <div className="">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {/*Card*/}
-        <div className="bg-secondary-100 p-8 rounded-xl">
-          <div className="flex items-center justify-between mb-4">
-            <div>
-              <RiTicketLine className="text-4xl bg-pink-500/10 text-pink-500 p-2 box-content rounded-xl" />
-            </div>
-            <div>
-              <Menu
-                menuButton={
-                  <MenuButton className="flex items-center gap-x-2 hover:bg-secondary-900 p-2 rounded-lg transition-colors">
-                    <RiMore2Fill />
-                  </MenuButton>
-                }
-                align="end"
-                arrow
-                transition
-                menuClassName="bg-secondary-100 p-4"
+    <div className="bg-secondary-100 p-8 rounded-xl">
+      <div className="flex items-center justify-between mb-4">
+        <div>
+          <RiTicketLine
+            className={`text-4xl ${status} p-2 box-content rounded-xl`}
+          />
+        </div>
+        <div>
+          <Menu
+            menuButton={
+              <MenuButton className="flex items-center gap-x-2 hover:bg-secondary-900 p-2 rounded-lg transition-colors">
+                <RiMore2Fill />
+              </MenuButton>
+            }
+            align="end"
+            arrow
+            transition
+            menuClassName="bg-secondary-100 p-4"
+          >
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link
+                to="/profile"
+                className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
               >
-                <MenuItem className="p-0 hover:bg-transparent">
-                  <Link
-                    to="/profile"
-                    className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
-                  >
-                    Show tickets
-                 </Link>
-                </MenuItem>
-                <MenuItem className="p-0 hover:bg-transparent">
-                  <Link
-                    to="/profile"
-                    className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
-                  >
-                    Information
-                 </Link>
-                </MenuItem>
-              </Menu>
-            </div>
-          </div>
-          {/*Number of tickets*/}
-          <div>
-            <h1 className="text-4xl text-white font-bold mb-4">145,000</h1>
-            <p>Total Tickets </p>
-          </div>
+                Show tickets
+              </Link>
+            </MenuItem>
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link
+                to="/profile"
+                className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
+              >
+                Information
+              </Link>
+            </MenuItem>
+          </Menu>
         </div>
       </div>
+      {/*Number of tickets*/}
+      <div>
+        <h1 className="text-4xl text-white font-bold mb-4">{totalTickets}</h1>
+        <p className={textColor}>{text} </p>
+      </div>
+      <hr className=" border border-dashed border-gray-500/50 my-4" />
+      <div>
+        <Link
+          to="/"
+          className="flex items-center gap-2 text-white hover:underline"
+        >
+          <RiAddLine />
+          Add new ticket
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardTicket
+CardTicket.propTypes = {
+  ticket: PropTypes.oneOf(["pending", "inProcess", "closed", "total"])
+    .isRequired,
+  totalTickets: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default CardTicket;
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -1,10 +1,198 @@
 import CardTicket from "../../components/CardTicket";
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/slide.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols">
-      <CardTicket />   
+      <div className="flex items-center justify-between mb-10">
+        <h1 className="text-4xl text-white">Good morning, Edinson!</h1>
+        <div className="flex items-center gap-2 text-3xl">
+          <RiArrowLeftSLine className="hover:cursor-pointer hover:text-white transition-colors" />
+          <RiArrowRightSLine className="hover:cursor-pointer hover:text-white transition-colors" />
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        {/*Card*/}
+        <CardTicket
+          ticket="total"
+          totalTickets="145,000"
+          text="Tickets totals"
+        />
+        <CardTicket
+          ticket="pending"
+          totalTickets="5,000"
+          text="Tickets pending"
+        />
+        <CardTicket
+          ticket="inProcess"
+          totalTickets="130,000"
+          text="Tickets in process"
+        />
+        <CardTicket
+          ticket="closed"
+          totalTickets="10,000"
+          text="Tickets closed"
+        />
+      </div>
+      <div className="text-2xl text-white my-10">
+        <h1>Recent Tickets</h1>
+      </div>
+      <div className="bg-secondary-100 p-8 rounded-xl">
+        <div className="hidden md:grid grid-cols-1 md:grid-cols-5 gap-4 mb-10">
+          <h5>ID</h5>
+          <h5>Description</h5>
+          <h5>Status</h5>
+          <h5>Date</h5>
+          <h5>Actions</h5>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 items-center mb-4 bg-secondary-900 p-4 rounded-xl">
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">ID</h5>
+            <span>#25</span>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Description</h5>
+            <p>My computer is not working</p>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Status</h5>
+            <span className="py-1 px-2 bg-yellow-500/10 text-yellow-500 rounded-lg">
+              Open
+            </span>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Date</h5>
+            <span>07 marzo 2024</span>
+          </div>
+          <Menu
+            menuButton={
+              <MenuButton className="flex items-center gap-x-2 bg-secondary-100 p-2 rounded-lg transition-colors">
+                Actions
+              </MenuButton>
+            }
+            align="end"
+            arrow
+            transition
+            menuClassName="bg-secondary-100 p-4"
+          >
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link
+                to="/profile"
+                className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
+              >
+                Show tickets
+              </Link>
+            </MenuItem>
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link
+                to="/profile"
+                className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
+              >
+                Information
+              </Link>
+            </MenuItem>
+          </Menu>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 items-center mb-4 bg-secondary-900 p-4 rounded-xl">
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">ID</h5>
+            <span>#25</span>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Description</h5>
+            <p>My computer is not working</p>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Status</h5>
+            <span className="py-1 px-2 bg-blue-500/10 text-yellow-500 rounded-lg">
+              In process
+            </span>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Date</h5>
+            <span>07 marzo 2024</span>
+          </div>
+          <Menu
+            menuButton={
+              <MenuButton className="flex items-center gap-x-2 bg-secondary-100 p-2 rounded-lg transition-colors">
+                Actions
+              </MenuButton>
+            }
+            align="end"
+            arrow
+            transition
+            menuClassName="bg-secondary-100 p-4"
+          >
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link
+                to="/profile"
+                className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
+              >
+                Show tickets
+              </Link>
+            </MenuItem>
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link
+                to="/profile"
+                className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
+              >
+                Information
+              </Link>
+            </MenuItem>
+          </Menu>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 items-center mb-4 bg-secondary-900 p-4 rounded-xl">
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">ID</h5>
+            <span>#25</span>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Description</h5>
+            <p>My computer is not working</p>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Status</h5>
+            <span className="py-1 px-2 bg-green-500/10 text-yellow-500 rounded-lg">
+              closed
+            </span>
+          </div>
+          <div>
+            <h5 className="md:hidden text-white font-bold mb-2">Description</h5>
+            <span>07 marzo 2024</span>
+          </div>
+          <Menu
+            menuButton={
+              <MenuButton className="flex items-center gap-x-2 bg-secondary-100 p-2 rounded-lg transition-colors">
+                Actions
+              </MenuButton>
+            }
+            align="end"
+            arrow
+            transition
+            menuClassName="bg-secondary-100 p-4"
+          >
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link
+                to="/profile"
+                className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
+              >
+                Show tickets
+              </Link>
+            </MenuItem>
+            <MenuItem className="p-0 hover:bg-transparent">
+              <Link
+                to="/profile"
+                className=" rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 p-2 flex-1"
+              >
+                Information
+              </Link>
+            </MenuItem>
+          </Menu>
+        </div>
       </div>
     </div>
   );
